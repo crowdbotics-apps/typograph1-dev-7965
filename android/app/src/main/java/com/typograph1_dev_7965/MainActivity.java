f586d1da3fe16563827f3291508d5e4452f7d8c0
Crowdbotics <team@crowdbotics.com>
2020-07-21 16:28:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/typograph1_dev_7965/MainActivity.java
@@ -0,0 +1,15 @@
+package com.typograph1_dev_7965;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "typograph1_dev_7965";
+    }
+}
